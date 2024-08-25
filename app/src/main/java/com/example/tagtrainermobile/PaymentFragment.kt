05f9b0f6fb70bdd57c02ddc704d845fc36efce56
João Lima <126878623+jpclima96@users.noreply.github.com>
2024-08-25 13:16:40 -0300

    adição do begin checkout e purchase

--- app/src/main/java/com/example/tagtrainermobile/PaymentFragment.kt
+++ app/src/main/java/com/example/tagtrainermobile/PaymentFragment.kt
@@ -5,68 +5,83 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.RadioButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.tagtrainermobile.models.User
+import com.example.tagtrainermobile.models.Product
 import com.google.android.material.textfield.TextInputLayout
-
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 class PaymentFragment : Fragment(), CartActivity.setRadioButtonsConfig {
 
-    val user = User.sigleUser.instance
+    private val user = User.sigleUser.instance
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_payment, container, false)
-        return view
+    private val cartProducts: List<Product> = Product.SingleCart.singleCartinstance
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_payment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        firebaseAnalytics = Firebase.analytics
+
         setProgressBar()
         setRadioButtonsConfig()
     }
 
-    fun setProgressBar() {
-        val progressBar = getView()?.findViewById<ProgressBar>(R.id.progressBar)
-        if(user.isLogged) {
-            progressBar?.progress = 66
-        } else {
-            progressBar?.progress = 33
+    private fun setProgressBar() {
+        val progressBar = view?.findViewById<ProgressBar>(R.id.progressBar)
+        progressBar?.progress = if (user.isLogged) 66 else 33
+    }
+
+    override fun setRadioButtonsConfig() {
+        val btnView = view?.findViewById<RadioButton>(R.id.bankSlipId)
+        val txtDados = view?.findViewById<TextView>(R.id.dataConfId)
+        val inputName = view?.findViewById<TextInputLayout>(R.id.textInputLayout)
+        val inputCpf = view?.findViewById<TextInputLayout>(R.id.cpfInputLayout)
+        val buttonFinish = view?.findViewById<Button>(R.id.finalizarCompraBkslp)
+
+        btnView?.setOnClickListener {
+            if (btnView.isChecked) {
+                txtDados?.visibility = View.VISIBLE
+                inputName?.visibility = View.VISIBLE
+                inputCpf?.visibility = View.VISIBLE
+                buttonFinish?.visibility = View.VISIBLE
+
+                buttonFinish?.setOnClickListener {
+                    val transactionFragment = TransactionFragment()
+                    fragmentManager?.beginTransaction()?.replace(R.id.fragmentPaymentId, transactionFragment)?.commit()
+
+                    logBeginCheckoutEvent()
+                }
+            }
         }
     }
 
-        override fun setRadioButtonsConfig() {
-            val btnView = getView()?.findViewById<RadioButton>(R.id.bankSlipId)
-            val txtDados  = getView()?.findViewById<TextView>(R.id.dataConfId)
-            val inputName = getView()?.findViewById<TextInputLayout>(R.id.textInputLayout)
-            val inputCpf =  getView()?.findViewById<TextInputLayout>(R.id.cpfInputLayout)
-            val buttonFinish = getView()?.findViewById<Button>(R.id.finalizarCompraBkslp)
-            if (btnView != null) {
-                btnView.setOnClickListener(object : View.OnClickListener {
-                    override fun onClick(view: View?) {
-                        if (btnView.isChecked) {
-                            txtDados?.visibility = View.VISIBLE
-                            inputName?.visibility = View.VISIBLE
-                            inputCpf?.visibility = View.VISIBLE
-                            buttonFinish?.visibility = View.VISIBLE
-
-                            buttonFinish?.setOnClickListener {
-                                val transactionFragment = TransactionFragment()
-                                getFragmentManager()?.beginTransaction()?.add(R.id.fragmentPaymentId, transactionFragment)?.commit()
-//                                val intent = Intent(context, PurchaseActivity::class.java)
-//                                startActivity(intent)
-//                                Toast.makeText(getActivity(), "oiioi", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                    }
-                })
-            }
+    private fun logBeginCheckoutEvent() {
+        val params = Bundle().apply {
+            putString(FirebaseAnalytics.Param.ITEM_LIST_NAME, "Cart")
+            putDouble(FirebaseAnalytics.Param.VALUE, calculateTotalValue()) // Calcule o valor total da compra
         }
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.BEGIN_CHECKOUT, params)
+    }
 
-
-
-
+    private fun calculateTotalValue(): Double {
+        var totalValue = 0.0
+        for (product in cartProducts) {
+            totalValue += product.price * product.quantity
+        }
+        return totalValue
+    }
 }
